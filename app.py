4bb0d0fedde324722e3ef69019c7e92fd9c357ae
Shivangx01b <46749216+Shivangx01b@users.noreply.github.com>
2025-01-07 19:46:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,15 +151,15 @@ def generate_response(prompt):
                     request_response = executor_agent(curl_to_python)
                     if "Failed to execute curl request as python code with error as" not in request_response:
                         print(f"[~] Request Response is : {request_response}")
-                        #validated_curl = validate_curl(curl_data=curl_raw, curl_response=request_response)
-                        #if "Failed to Validate curl request due to error" not in validated_curl:
-                        validated_curl = curl_raw
-                        print(f"[~] Final Correct curl is : {validated_curl}")
-                        #final_response = final_bot_answer(validated_curl, res["answer"])
-                        final_response = res["answer"]
-                        return final_response
-                        #else:
-                        #    print(validated_curl)
+                        validated_curl = validate_curl(curl_data=curl_raw, curl_response=request_response)
+                        if "Failed to Validate curl request due to error" not in validated_curl:
+                            validated_curl = curl_raw
+                            print(f"[~] Final Correct curl is : {validated_curl}")
+                            final_response = final_bot_answer(validated_curl, res["answer"])
+                            
+                            return final_response
+                        else:
+                            print(validated_curl)
                     else:
                         print(request_response)
                 else:
